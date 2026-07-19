2e15d2c05f9ddd364648dc1aaeaefec2a4f86227
Ikanyeng Adams <ikanyengadams2@gmail.com>
2026-07-19 13:52:13 +0200

    learned about inputs

---
+++ 03_vars_outputs/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.7.0"
+    }
+  }
+}
+
+# resource "local_file" "example1" {
+#   content  = "foo!"
+#   filename = "${path.module}/${var.filename-1}.txt"
+#   count = var.count_num
+# }
+
+# locals {
+#   base_path = "${path.module}/files"
+# }
+
+# resource "local_file" "example2" {
+#   content  = "foo!"
+#   filename = "${local.base_path}/example2.md"
+# }
+
+# resource "local_file" "example3" {
+#   content  = "foo!"
+#   filename = "${local.base_path}/example3.md"
+# }
+
+# resource "local_file" "example4" {
+#   content  = "foo!"
+#   filename = "${local.base_path}/example4.md"
+# }
+
+
+locals {
+  environment = "prod" # dev, staging, prod
+  upper_case  = upper(local.environment)
+  base_path   = "${path.module}/configs/${local.upper_case}"
+}
+
+resource "local_file" "service_configs" {
+  filename = "${local.base_path}/server.sh"
+  content  = <<EOT
+  environment = ${local.environment}
+  port=3000
+  EOT
+}
+
+resource "local_file" "service_configs2" {
+  filename = "${local.base_path}/server2.sh"
+  content  = <<EOT
+  environment = ${local.environment}
+  port=3000
+  EOT
+}
+
+resource "local_file" "service_configs3" {
+  filename = "${local.base_path}/server3.sh"
+  content  = <<EOT
+  environment = ${local.environment}
+  port=3000
+  EOT
+}
---
+++ 03_vars_outputs/variables.tf
@@ -0,0 +1,22 @@
+variable "filename-1" {
+  description = "this is the name of file 1"
+  default = "example1"
+  type = string
+}
+
+variable "filename-2" {
+  description = "this is the name of file 2"
+  default = "example2"
+  type = string
+}
+
+variable "filename-3" {
+  description = "this is the name of file 3"
+  default = "example3"
+  type = string
+}
+
+variable "count_num" {
+  type = number
+  default = 1
+}
